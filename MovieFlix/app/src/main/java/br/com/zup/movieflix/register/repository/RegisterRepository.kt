1e38640b69938cc33d88314a453169d7ae84563d
jennysival <jeniffersival@gmail.com>
2022-06-24 17:48:53 -0300

    criar função no repository

--- MovieFlix/app/src/main/java/br/com/zup/movieflix/register/repository/RegisterRepository.kt
+++ MovieFlix/app/src/main/java/br/com/zup/movieflix/register/repository/RegisterRepository.kt
@@ -1,4 +1,13 @@
 package br.com.zup.movieflix.register.repository
 
+import br.com.zup.movieflix.register.model.RegisterModel
+
 class RegisterRepository {
+
+    fun createAccount(userRegister: RegisterModel): RegisterModel{
+
+        //verificar se as duas senhas digitadas são iguais
+
+        return userRegister
+    }
 }
